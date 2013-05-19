bd5dd5c15c3b45ddba47ae56eac0b15664055c0f
Karen Tsai <angelXwind@angelxwind.net>
2013-05-19 13:54:37 -0300

    Okay, that didn't work.

--- Classes/EmuViewController.mm
+++ Classes/EmuViewController.mm
@@ -262,16 +262,16 @@ const float textureVert[] =
     UIButton* buttonRight = [UIButton buttonWithId:BUTTON_RIGHT atCenter:CGPointMake(100, 172)];
     [self.view addSubview:buttonRight];
     
-    UIButton* buttonY = [UIButton buttonWithId:BUTTON_Y atCenter:CGPointMake(219, 173)];
+    UIButton* buttonX = [UIButton buttonWithId:BUTTON_X atCenter:CGPointMake(219, 173)];
     [self.view addSubview:buttonX];
     
-    UIButton* buttonX = [UIButton buttonWithId:BUTTON_X atCenter:CGPointMake(259, 132)];
+    UIButton* buttonY = [UIButton buttonWithId:BUTTON_Y atCenter:CGPointMake(259, 132)];
     [self.view addSubview:buttonY];
     
-    UIButton* buttonB = [UIButton buttonWithId:BUTTON_B atCenter:CGPointMake(259, 211)];
+    UIButton* buttonA = [UIButton buttonWithId:BUTTON_A atCenter:CGPointMake(259, 211)];
     [self.view addSubview:buttonA];
     
-    UIButton* buttonA = [UIButton buttonWithId:BUTTON_A atCenter:CGPointMake(299, 173)];
+    UIButton* buttonB = [UIButton buttonWithId:BUTTON_B atCenter:CGPointMake(299, 173)];
     [self.view addSubview:buttonB];
     
     UIButton* buttonSelect = [UIButton buttonWithId:BUTTON_SELECT atCenter:CGPointMake(132, 228)];
@@ -360,5 +360,4 @@ const float textureVert[] =
     
 }
 @end
-
 
